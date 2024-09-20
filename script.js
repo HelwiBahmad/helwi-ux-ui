0bcd47db92edb933ebd7ebffde72ba80f0165ae4
HelwiBahmad <161090130+HelwiBahmad@users.noreply.github.com>
2024-09-20 20:37:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -100,8 +100,6 @@ function startCountdown(endDate) {
     const interval = setInterval(updateTimer, 1000);
 }
 
-const endDate = new Date('2024-09-07T00:00:00').getTime();
-startCountdown(endDate);
 
 
 document.getElementById('cadastroForm').addEventListener('submit', function(event) {
